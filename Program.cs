11899fed1851a0fee208c2789588bbd760e74ab5
znoux46 <trnhn46@gmail.com>
2025-10-03 22:22:35 +0700

    uu oke no

--- Program.cs
+++ Program.cs
@@ -30,7 +30,7 @@ namespace Products_Management
                 options.AddPolicy("AllowReactApp",
                     policy =>
                     {
-                        policy.WithOrigins("http://localhost:3000","https://asignment1-frontend-e1xm.vercel.app/")
+                        policy.WithOrigins("http://localhost:3000","https://asignment1-frontend-e1xm.vercel.app")
                               .AllowAnyHeader()
                               .AllowAnyMethod();
                     });
