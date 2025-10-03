c0d85bba7e0fd4366d63489a64fc9988924bb267
znoux46 <trnhn46@gmail.com>
2025-10-03 19:13:06 +0700

    [FEAT] - API Src code

---
+++ API/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Products_Management.API
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<Entity> Entities { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new EntityConfiguration());
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
